45b8498e59d239705180866260e4d30c84ce7a6f
wblanken <blanks821@gmail.com>
2014-04-24 17:24:23 -0500

    Laid out basic methods

--- Challenges/Easy/Easy_159/Program.cs
+++ Challenges/Easy/Easy_159/Program.cs
@@ -9,6 +9,56 @@ namespace Easy_159
     {
         static void Main(string[] args)
         {
+            MainLoop();
         }
+
+        private static void MainLoop()
+        {
+            // TOOD: Welcome message
+
+            // TODO: Loop
+
+            // TODO: Exit stats display
+        }
+
+
+        private static WinState Rules(Hand inputOne, Hand inputTwo)
+        {
+            WinState oneBeatsTwo = WinState.Tie; 
+
+            switch (inputOne)
+            {
+                case Hand.Rock:
+                    break;
+                case Hand.Paper:
+                    break;
+                case Hand.Scissors:
+                    break;
+                case Hand.Lizzard:
+                    break;
+                case Hand.Spock:
+                    break;
+                default:
+                    oneBeatsTwo = WinState.Tie;
+                    break;
+            }
+
+            return oneBeatsTwo;
+        }
+    }
+
+    public enum WinState
+    {
+        Tie = -1,
+        Loss = 0,
+        Win = 1                
+    }
+    public enum Hand
+    {
+        Rock,
+        Paper,
+        Scissors,
+        Lizzard,
+        Spock
     }
 }
