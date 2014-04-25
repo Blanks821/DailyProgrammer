fda913740d55d4ae7de755cff0e3fe180a3035d4
wblanken <blanks821@gmail.com>
2014-04-24 23:55:26 -0500

    Added game rule processing

--- Challenges/Easy/Easy_159/Program.cs
+++ Challenges/Easy/Easy_159/Program.cs
@@ -21,33 +21,137 @@ namespace Easy_159
             // TODO: Exit stats display
         }
 
-
-        private static WinState Rules(Hand inputOne, Hand inputTwo)
+        private static WinState Rules(Hand playerOne, Hand playerTwo)
         {
-            WinState oneBeatsTwo = WinState.Tie; 
-
-            switch (inputOne)
+            WinState oneBeatsTwo = new WinState();
+            if (playerOne == playerTwo)
             {
-                case Hand.Rock:
-                    break;
-                case Hand.Paper:
-                    break;
-                case Hand.Scissors:
-                    break;
-                case Hand.Lizzard:
-                    break;
-                case Hand.Spock:
-                    break;
-                default:
-                    oneBeatsTwo = WinState.Tie;
-                    break;
+                oneBeatsTwo.Outcome = Outcome.Tie;
+                oneBeatsTwo.Message = GameMessages.tie;
             }
-
+            else
+            {
+                switch (playerOne)
+                {
+                    case Hand.Rock:
+                        switch (playerTwo)
+                        {
+                            case Hand.Paper:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.paperVRock;
+                                break;
+                            case Hand.Scissors:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.rockVScissors;
+                                break;
+                            case Hand.Lizard:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.rockVLizard;
+                                break;
+                            case Hand.Spock:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.spockVRock;
+                                break;
+                        }
+                        break;
+                    case Hand.Paper:
+                        switch (playerTwo)
+                        {
+                            case Hand.Rock:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.paperVRock;
+                                break;
+                            case Hand.Scissors:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.scissorsVPaper;
+                                break;
+                            case Hand.Lizard:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.lizardVPaper;
+                                break;
+                            case Hand.Spock:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.paperVSpock;
+                                break;
+                        }
+                        break;
+                    case Hand.Scissors:
+                        switch (playerTwo)
+                        {
+                            case Hand.Rock:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.rockVScissors;
+                                break;
+                            case Hand.Paper:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.scissorsVPaper;
+                                break;
+                            case Hand.Lizard:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.scissorsVLizard;
+                                break;
+                            case Hand.Spock:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.spockVScissors;
+                                break;
+                        }
+                        break;
+                    case Hand.Lizard:
+                        switch (playerTwo)
+                        {
+                            case Hand.Rock:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.rockVLizard;
+                                break;
+                            case Hand.Paper:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.lizardVPaper;
+                                break;
+                            case Hand.Scissors:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.scissorsVLizard;
+                                break;
+                            case Hand.Spock:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.lizardVSpock;
+                                break;
+                        }
+                        break;
+                    case Hand.Spock:
+                        switch (playerTwo)
+                        {
+                            case Hand.Rock:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.spockVRock;
+                                break;
+                            case Hand.Paper:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.paperVSpock;
+                                break;
+                            case Hand.Scissors:
+                                oneBeatsTwo.Outcome = Outcome.Win;
+                                oneBeatsTwo.Message = GameMessages.spockVScissors;
+                                break;
+                            case Hand.Lizard:
+                                oneBeatsTwo.Outcome = Outcome.Loss;
+                                oneBeatsTwo.Message = GameMessages.lizardVSpock;
+                                break;
+                        }
+                        break;
+                }
+            }
             return oneBeatsTwo;
         }
     }
+    public struct WinState
+    {
+        private Outcome outcome;
+        private string message;
 
-    public enum WinState
+        public Outcome Outcome { get; set; }
+        public string Message { get; set; }
+    }
+    public enum Outcome
     {
         Tie = -1,
         Loss = 0,
@@ -58,7 +162,21 @@ namespace Easy_159
         Rock,
         Paper,
         Scissors,
-        Lizzard,
+        Lizard,
         Spock
+    }
+    public static struct GameMessages
+    {
+        public static readonly string tie = "It's a tie";
+        public static readonly string paperVRock = "Paper covers rock";
+        public static readonly string paperVSpock = "Paper disproves Spock";
+        public static readonly string rockVScissors = "Rock crushes scissors";
+        public static readonly string rockVLizard = "Rock crushes lizard";
+        public static readonly string scissorsVPaper = "Scissors cut paper";
+        public static readonly string scissorsVLizard = "Scissors decapitate lizard";
+        public static readonly string lizardVPaper = "Lizard eats paper";
+        public static readonly string lizardVSpock = "Lizard poisons Spock";
+        public static readonly string spockVRock = "Spock vaporizes rock";
+        public static readonly string spockVScissors = "Spock smashes scissors";
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ DailyProgrammer
 ===============
 
 My C# solutions for /r/dailyprogrammer 
+
+
+#159: http://www.reddit.com/r/dailyprogrammer/comments/23lfrf/4212014_challenge_159_easy_rock_paper_scissors/
