24c405417dd6de930fb8870e1d9beeae55f4e975
wblanken <blanks821@gmail.com>
2014-04-25 11:46:49 -0500

    Majority of the game structure is in place.

--- Challenges/Easy/Easy_159/Program.cs
+++ Challenges/Easy/Easy_159/Program.cs
@@ -14,10 +14,60 @@ namespace Easy_159
 
         private static void MainLoop()
         {
-            // TOOD: Welcome message
+            Hand playerOne, playerTwo;
+            WinState outcome;
+            bool done = false;
 
-            // TODO: Loop
+            Console.WriteLine("Welcome to Rock Paper Scissors Lizard Spock!");
 
+            // Game Loop
+            while (!done)
+            {
+                Console.WriteLine("Please make a selection:");
+                Console.WriteLine("\t1)Rock\n\t2)Paper\n\t3)Scissors\n\t4)Lizard\n\t5)Spock\nPress Q to Quit...");
+                char input = Convert.ToChar(Console.Read());
+
+                playerOne = Hand.Invalid;
+
+                switch (input)
+                {
+                    case 'q': 
+                    case 'Q':
+                        done = true;
+                        break;
+                    case '1':
+                        playerOne = Hand.Rock;
+                        break;
+                    case '2':
+                        playerOne = Hand.Paper;
+                        break;
+                    case '3':
+                        playerOne = Hand.Scissors;
+                        break;
+                    case '4':
+                        playerOne = Hand.Lizard;
+                        break;
+                    case '5':
+                        playerOne = Hand.Spock;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection");
+                        playerOne = Hand.Invalid;
+                        continue;
+                }
+
+                if (playerOne == Hand.Invalid) // probably overkill but just in case
+                {
+                    Console.WriteLine("Invalid selection");
+                    continue;
+                }
+
+                Random rand = new Random();
+                playerTwo = (Hand)rand.Next(1, 5);
+
+                GameStats.AddGame(outcome = Rules(playerOne, playerTwo));
+                Console.WriteLine("{0}!\n{1}", outcome.Message, outcome.Outcome == Outcome.Win ? GameMessages.win : GameMessages.lose);
+            }
             // TODO: Exit stats display
         }
 
@@ -143,11 +193,9 @@ namespace Easy_159
             return oneBeatsTwo;
         }
     }
+
     public struct WinState
     {
-        private Outcome outcome;
-        private string message;
-
         public Outcome Outcome { get; set; }
         public string Message { get; set; }
     }
@@ -159,13 +207,14 @@ namespace Easy_159
     }
     public enum Hand
     {
+        Invalid = 0,
         Rock,
         Paper,
         Scissors,
         Lizard,
         Spock
     }
-    public static struct GameMessages
+    public struct GameMessages
     {
         public static readonly string tie = "It's a tie";
         public static readonly string paperVRock = "Paper covers rock";
@@ -178,5 +227,49 @@ namespace Easy_159
         public static readonly string lizardVSpock = "Lizard poisons Spock";
         public static readonly string spockVRock = "Spock vaporizes rock";
         public static readonly string spockVScissors = "Spock smashes scissors";
+        public static readonly string win = "You win!";
+        public static readonly string lose = "Sorry, you lose.";
+    }
+    public struct GameStats
+    {
+        private static int humanWins;
+        private static int computerWins;
+        private static int ties;
+        private static int totalGames;
+        private static bool initalized = false;
+
+        public static int HumanWins { get { return humanWins; } }
+        public static int ComputerWins { get { return computerWins; } }
+        public static int Ties { get { return ties; } }
+        public static int TotalGames { get { return totalGames; } }
+
+        public static void Start()
+        {
+            if (!initalized)
+            {
+                humanWins = 0;
+                computerWins = 0;
+                ties = 0;
+                totalGames = 0;
+            }
+            initalized = true;
+        }
+
+        public static void AddGame(WinState outcome)
+        {
+            totalGames++;
+            switch (outcome.Outcome)
+            {
+                case Outcome.Win:
+                    humanWins++;
+                    break;
+                case Outcome.Tie:
+                    ties++;
+                    break;
+                case Outcome.Loss:
+                    computerWins++;
+                    break;
+            }
+        }        
     }
 }
